a9dab600a4492685e142282530868d396acc1ca3
Boris Kachanov <mr.kachanov@gmail.com>
2023-11-11 16:31:38 +0200

    fix test

--- frontend/src/App.test.tsx
+++ frontend/src/App.test.tsx
@@ -1,8 +1,14 @@
-import { render, screen } from '@testing-library/react';
-import { App } from './App';
+import { render, screen } from "@testing-library/react";
+import { BrowserRouter } from "react-router-dom";
+import { App } from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText('Junction 2023!');
-  expect(linkElement).toBeInTheDocument();
+test("smoke test", () => {
+  render(
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  );
+  const Logo = screen.getByAltText("Logo");
+  
+  expect(Logo).toBeInTheDocument();
 });

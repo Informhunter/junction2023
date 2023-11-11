3d8ab5816d3af517e9a4842b05006ee2a02fc7a9
Boris Kachanov <mr.kachanov@gmail.com>
2023-11-11 09:48:23 +0200

    hotfix api endpoint

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -2,7 +2,7 @@ import axios, { AxiosResponse } from "axios";
 
 export async function getRandomNumber() {
   return axios
-    // .get<{ number: number }>("http://192.168.1.127:8080/api/random")
-    .get<{ number: number }>("http://10.0.13.146:8080/random")
+    .get<{ number: number }>("http://192.168.1.127:8080/api/random")
+    // .get<{ number: number }>("http://10.0.13.146:8080/random")
     .then(({ data }) => data);
 }

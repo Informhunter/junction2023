8cc9505a10cc2f3164f8a71aef6f0a4b6a1bf3d7
Alexandr Lukoyanov <alexandr@vainu.io>
2023-11-11 15:32:20 +0200

    Add severity level and filtration

---
+++ backend/src/llm/severity_level.py
@@ -0,0 +1,61 @@
+from enum import Enum
+
+
+class SeverityLevel(Enum):
+    def __new__(cls, value: str, description: str, examples: list[str]):
+        obj = object.__new__(cls)
+        obj._value_ = value
+        obj.description = description
+        obj.examples = examples
+        return obj
+
+    NO_PROBLEM = (
+        'no_problem',
+        "Trivial issues or wishes with no negative impact on the individual's emotional or physical well-being.",
+        [
+            'Wanting to try a new ice cream flavor',
+            'Feeling a bit bored and wanting to find a new hobby',
+            'Wishing for nicer weather',
+            'Hoping to go to the zoo or a museum',
+        ],
+    )
+    LOW = (
+        'low',
+        'Minor everyday problems that are slightly bothersome '
+        'but manageable and do not significantly affect well-being.',
+        [
+            'Misplacing house keys',
+            'Having a small disagreement with a friend',
+        ],
+    )
+    MODERATE = (
+        'moderate',
+        'Issues that have a noticeable negative effect on well-being, '
+        'may require effort to resolve, and can cause moderate distress.',
+        [
+            'Struggling with a work-life balance',
+            'Dealing with a persistent health issue',
+            'Facing a difficult personal relationship',
+            'Experiencing financial strain',
+        ],
+    )
+    CRITICAL = (
+        'critical',
+        'Serious, urgent problems that pose a significant risk to life or well-being, '
+        'require immediate intervention, and can include mental health crises.',
+        [
+            'Having thoughts of self-harm or suicide',
+            'Experiencing a severe illness or injury',
+            'Being in a situation of immediate danger or abuse',
+            'Suffering from a significant loss or intense grieving',
+        ],
+    )
+
+    @classmethod
+    def to_markdown_table(cls):
+        markdown_output = '| Severity | Description | Examples |\n'
+        markdown_output += '| --- | --- | --- |\n'
+        for level in cls:
+            examples_md = '<br>'.join(level.examples)
+            markdown_output += f'| {level.value} | {level.description} | {examples_md} |\n'
+        return markdown_output.strip()
--- backend/src/models.py
+++ backend/src/models.py
@@ -1,6 +1,9 @@
 from pydantic import BaseModel
 
+from src.llm.severity_level import SeverityLevel
+
 
 class HowtoSuggestion(BaseModel):
     paragraph_id: int
+    severity_level: SeverityLevel
     text: str

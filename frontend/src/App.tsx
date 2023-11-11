dcbb613f328db6e7914b4fcd7fa6322585e2fc9f
Boris Kachanov <mr.kachanov@gmail.com>
2023-11-11 10:45:48 +0200

    add core component

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { QueryClient, QueryClientProvider } from "react-query";
 import { Typography } from "@mui/material";
 import { Authors } from "./Authors";
 import { RandomNumber } from "./RandomNumber";
+import { Core } from "./Core";
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -18,6 +19,7 @@ const App: React.FC = () => {
       <Typography variant='h2'>Junction 2023!</Typography>
       <Authors />
       <RandomNumber />
+      <Core />
     </QueryClientProvider>
   );
 }

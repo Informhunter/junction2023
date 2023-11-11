9d9bf2c3e3bd62a28e7478f62dce68890230e5db
Boris Kachanov <mr.kachanov@gmail.com>
2023-11-11 13:26:17 +0200

    fix prod requests urls

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -3,8 +3,8 @@ import axios from "axios";
 export async function getRandomNumber() {
   return (
     axios
-      // .get<{ number: number }>("/api/random")
-      .get<{ number: number }>("http://10.0.13.146:8080/random")
+      .get<{ number: number }>("/api/healthcheck")
+      // .get<{ number: number }>("http://10.0.13.146:8080/healthcheck")
       .then(({ data }) => data)
   );
 }
@@ -16,8 +16,8 @@ interface Suggestion {
 export async function sendNote(note: string) {
   return (
     axios
-      // .post<Suggestion[]>("api/note", note, {
-      .post<Suggestion[]>("http://10.0.13.146:8080/note", note, {
+      .post<Suggestion[]>("api/note", note, {
+      // .post<Suggestion[]>("http://10.0.13.146:8080/note", note, {
         headers: { "Content-Type": "text/plain" },
       })
       .then(({ data }) => data)

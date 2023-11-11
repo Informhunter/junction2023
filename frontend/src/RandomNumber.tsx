87d32670629bac1d477ddbfa0eda74275034fe5e
Boris Kachanov <mr.kachanov@gmail.com>
2023-11-11 09:00:06 +0200

    minor stuff

--- frontend/src/RandomNumber.tsx
+++ frontend/src/RandomNumber.tsx
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import { getRandomNumber } from './api';
+import React, { useState, useEffect } from "react";
+import { getRandomNumber } from "./api";
 
 function RandomNumber() {
-  const [randomNumber, setRandomNumber] = useState<number | undefined>(undefined);
-  
+  const [randomNumber, setRandomNumber] = useState<number | undefined>(
+    undefined
+  );
+
   useEffect(() => {
-    getRandomNumber().then(({ data }) => setRandomNumber(data.number));
+    getRandomNumber().then(({ number }) => setRandomNumber(number));
   }, []);
 
-  return (
-    <h2>Random number: {randomNumber}</h2>
-  )
-};
+  return <h2>Random number: {randomNumber}</h2>;
+}
 
 export { RandomNumber };

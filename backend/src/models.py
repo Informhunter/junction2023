8c165d68b11dbf98a0a11b7b38654e4f66394157
Informhunter <informhunter@gmail.com>
2023-11-12 08:08:03 +0200

    Minor changes.

--- backend/src/models.py
+++ backend/src/models.py
@@ -1,6 +1,12 @@
+from enum import Enum
 from pydantic import BaseModel
 
 from src.llm.severity_level import SeverityLevel
+
+
+class SuggestionType(str, Enum):
+    CHIT_CHAT = 'chit-chat'
+    HOW_TO = 'how-to'
 
 
 class WikihowSearchResult(BaseModel):
@@ -9,7 +15,8 @@ class WikihowSearchResult(BaseModel):
     thumbnail_url: str | None
 
 
-class HowtoSuggestion(BaseModel):
+class Suggestion(BaseModel):
+    type: SuggestionType
     paragraph_id: int
     severity_level: SeverityLevel
     text: str
--- backend/src/server.py
+++ backend/src/server.py
@@ -8,7 +8,8 @@ from src.llm.extraction_chain import EXTRACTION_CHAIN
 from src.llm.chit_chat import CHIT_CHAT_CHAIN
 from src.llm.severity_level import SeverityLevel
 from src.models import (
-    HowtoSuggestion,
+    Suggestion,
+    SuggestionType,
     WikihowSearchResult,
 )
 from src.retriever import search_wikihow
@@ -25,17 +26,20 @@ async def random():
 
 
 @app.post('/note')
-async def suggest(note: Annotated[str, Body()]) -> list[HowtoSuggestion]:
+async def suggest(note: Annotated[str, Body()]) -> list[Suggestion]:
+
     if not note:
         return []
 
     suggestions = await _get_suggestions_with_llm(note)
+    if len(suggestions) > 0:
+        return suggestions
 
-    if len(suggestions) == 0:
-        return await _get_chit_chat_suggestion(note)
+    return await _get_chit_chat_suggestion(note)
 
 
-async def _get_suggestions_with_llm(note: str) -> list[HowtoSuggestion]:
+
+async def _get_suggestions_with_llm(note: str) -> list[Suggestion]:
     paragraphs = re.split(r'\n{2,}', note.strip())
 
     # TODO: not only last paragraph
@@ -52,7 +56,7 @@ async def _get_suggestions_with_llm(note: str) -> list[HowtoSuggestion]:
     return suggestions[:_N_SUGGESTIONS_TO_RETURN]
 
 
-async def _parse_llm_response(response: Sequence[dict[str, str]], paragraph_id: int) -> list[HowtoSuggestion]:
+async def _parse_llm_response(response: Sequence[dict[str, str]], paragraph_id: int) -> list[Suggestion]:
     suggestions = []
     for output in response:
         search_result = None
@@ -61,7 +65,8 @@ async def _parse_llm_response(response: Sequence[dict[str, str]], paragraph_id: 
             search_result = wikihow_results[0]
 
         suggestions.append(
-            HowtoSuggestion(
+            Suggestion(
+                type=SuggestionType.HOW_TO,
                 paragraph_id=paragraph_id,
                 search_result=search_result,
                 **output,
@@ -71,7 +76,7 @@ async def _parse_llm_response(response: Sequence[dict[str, str]], paragraph_id: 
     return suggestions
 
 
-def _filter_suggestions(suggestions: Iterable[HowtoSuggestion]) -> Iterable[HowtoSuggestion]:
+def _filter_suggestions(suggestions: Iterable[Suggestion]) -> Iterable[Suggestion]:
     for suggestion in suggestions:
         if (
             suggestion.text.lstrip().lower().startswith('how to')
@@ -80,14 +85,16 @@ def _filter_suggestions(suggestions: Iterable[HowtoSuggestion]) -> Iterable[Howt
             yield suggestion
 
 
-def _sort_suggestions(suggestions: Iterable[HowtoSuggestion]) -> list[HowtoSuggestion]:
+def _sort_suggestions(suggestions: Iterable[Suggestion]) -> list[Suggestion]:
     return sorted(suggestions, key=lambda suggestion: suggestion.severity_level, reverse=True)
 
-async def _get_chit_chat_suggestion(note: str) -> list[HowtoSuggestion]:
+
+async def _get_chit_chat_suggestion(note: str) -> list[Suggestion]:
     paragraphs = re.split(r'\n{2,}', note.strip())
     chit_chat = await _generate_chit_chat_suggestion(paragraphs)
     return [
-        HowtoSuggestion(
+        Suggestion(
+            type=SuggestionType.CHIT_CHAT,
             paragraph_id=len(paragraphs) - 1,
             text=paragraphs[-1],
             search_result=WikihowSearchResult(
@@ -98,6 +105,7 @@ async def _get_chit_chat_suggestion(note: str) -> list[HowtoSuggestion]:
             severity_level=SeverityLevel.NO_PROBLEM,
         ),
     ]
+
 
 async def _generate_chit_chat_suggestion(paragraphs: list[str]) -> str:
     response = await CHIT_CHAT_CHAIN.ainvoke(paragraphs[-1])

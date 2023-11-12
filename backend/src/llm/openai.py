76bb000e610487d40962206082a33a3e989ce402
Alexandr Lukoyanov <alexandr@vainu.io>
2023-11-12 08:57:01 +0200

    Refactoring

--- backend/src/llm/openai.py
+++ backend/src/llm/openai.py
@@ -7,7 +7,7 @@ _OPENAI_SECRET_KEY = os.environ.get('OPENAI_SECRET_KEY')
 _OPENAI_ORGANISATION_ID = os.environ.get('OPENAI_ORGANISATION_ID')
 
 
-OPENAI_GPT4_MODEL = ChatOpenAI(
+OPENAI_EXTRACTION_MODEL = ChatOpenAI(
     model_name='gpt-4-1106-preview',
     temperature=0,
     max_tokens=500,
@@ -15,10 +15,10 @@ OPENAI_GPT4_MODEL = ChatOpenAI(
     openai_api_key=_OPENAI_SECRET_KEY,
 )
 
-OPENAI_CHAT_GPT_MODEL = ChatOpenAI(
+OPENAI_SUMMARIZATION_MODEL = ChatOpenAI(
     model_name='gpt-3.5-turbo-1106',
     temperature=0,
-    max_tokens=500,
+    max_tokens=100,
     openai_organization=_OPENAI_ORGANISATION_ID,
     openai_api_key=_OPENAI_SECRET_KEY,
 )
